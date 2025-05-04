a52f1ce8def63404476267194e242aef28695450
Sylwester Dąbrowski <sdabrowski95@gmail.com>
2025-05-04 18:30:16 +0200

    The first commit

---
+++ generator/main.asm
@@ -0,0 +1,10 @@
+; Authors: Oskar Herman, Sylwester D¹browski
+; Project for Microprocessor thechnics class at AGH University of Krakow
+; 
+; Description : bla bla bla 
+
+
+
+start:
+    
+    rjmp start
